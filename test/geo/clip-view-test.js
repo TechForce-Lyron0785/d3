TechForce-Lyron0785/d3

commit a32c615f54333eebca9d46d6b38b1a1df2fadd41

Fix projection.clipExtent edge case.
A slight correction to clipLine fixed the problem with points lying
exactly on the clip edge, but in case clipLine(a, b) ever returns false
(no intersections) even though v_=0 and v=1, we treat this as the start
of a line anyway.

d3.js
@@ -2788,7 +2788,7 @@ d3 = function() {
     }
   }
   function d3_geo_clipViewT(num, denominator, t) {
-    if (Math.abs(denominator) < ε) return num < 0;
+    if (Math.abs(denominator) < ε) return num <= 0;
     var u = num / denominator;
     if (denominator > 0) {
       if (u > t[1]) return false;

src/geo/clip-view.js
@@ -134,6 +134,9 @@ function d3_geo_clipView(x0, y0, x1, y1) {
             }
             listener.point(b[0], b[1]);
             if (!v) listener.lineEnd();
+          } else {
+            listener.lineStart();
+            listener.point(x, y);
           }
         }
       }
@@ -197,7 +200,7 @@ function d3_geo_clipView(x0, y0, x1, y1) {
 }
 
 function d3_geo_clipViewT(num, denominator, t) {
-  if (Math.abs(denominator) < ε) return num < 0;
+  if (Math.abs(denominator) < ε) return num <= 0;
 
   var u = num / denominator;
 

commit 306c1c5a699fcee4e12ae20b2c43ad20b3ddedad

Fix projection.clipExtent regression.
A bug was introduced in a32c615f54333eebca9d46d6b38b1a1df2fadd41, where
invisible line segments were incorrectly drawn.

Thanks to @jfirebaugh of iD for spotting it!

d3.js
@@ -2752,7 +2752,7 @@ d3 = function() {
               }
               listener.point(b[0], b[1]);
               if (!v) listener.lineEnd();
-            } else {
+            } else if (v) {
               listener.lineStart();
               listener.point(x, y);
             }

src/geo/clip-view.js
@@ -136,7 +136,7 @@ function d3_geo_clipView(x0, y0, x1, y1) {
             }
             listener.point(b[0], b[1]);
             if (!v) listener.lineEnd();
-          } else {
+          } else if (v) {
             listener.lineStart();
             listener.point(x, y);
           }

test/geo/clip-view-mock.js
@@ -1,0 +1,4 @@
+import "../../src/geo/geo";
+import "../../src/geo/clip-view";
+
+d3.geo.clipView = d3_geo_clipView;

test/geo/clip-view-test.js
@@ -1,0 +1,44 @@
+var vows = require("vows"),
+    _ = require("../../"),
+    load = require("../load"),
+    assert = require("../assert");
+
+var suite = vows.describe("d3.geo.clipView");
+
+suite.addBatch({
+  "clipView": {
+    topic: load("../test/geo/clip-view-mock").expression("d3.geo.clipView"),
+    "100⨯100": {
+      topic: function(clipView) {
+        return clipView(0, 0, 100, 100);
+      },
+      "invisible segments are dropped": function(clip) {
+        _.geo.stream({type: "LineString", coordinates: [
+          [-100, 50],
+          [-50, 50],
+          [50, 50]
+        ]}, clip(testContext));
+        assert.deepEqual(testContext.buffer(), [
+          {type: "lineStart"},
+          {type: "point", x: 0, y: 50},
+          {type: "point", x: 50, y: 50},
+          {type: "lineEnd"}
+        ]);
+      }
+    }
+  }
+});
+
+suite.export(module);
+
+var testBuffer = [];
+
+var testContext = {
+  point: function(x, y) { testBuffer.push({type: "point", x: Math.round(x), y: Math.round(y)}); },
+  lineStart: function() { testBuffer.push({type: "lineStart"}); },
+  lineEnd: function() { testBuffer.push({type: "lineEnd"}); },
+  polygonStart: function() { testBuffer.push({type: "polygonStart"}); },
+  polygonEnd: function() { testBuffer.push({type: "polygonEnd"}); },
+  sphere: function() { testBuffer.push({type: "sphere"}); },
+  buffer: function() { var result = testBuffer; testBuffer = []; return result; }
+};