TechForce-Lyron0785/d3

commit 8c7cf85d299609a778cdb7c1d8afb5534b3f1d9b

Fix another rounding bug in log.ticks.

d3.js
@@ -6835,9 +6835,9 @@ d3 = function() {
       return scale;
     };
     scale.ticks = function() {
-      var extent = d3_scaleExtent(linear.domain()), ticks = [];
+      var extent = d3_scaleExtent(domain), ticks = [];
       if (extent.every(isFinite)) {
-        var i = Math.floor(extent[0]), j = Math.ceil(extent[1]), u = pow(extent[0]), v = pow(extent[1]), n = base % 1 ? 2 : base;
+        var i = Math.floor(log(extent[0])), j = Math.ceil(log(extent[1])), u = extent[0], v = extent[1], n = base % 1 ? 2 : base;
         if (positive) {
           for (;i < j; i++) for (var k = 1; k < n; k++) ticks.push(Math.pow(base, i) * k);
           ticks.push(Math.pow(base, i));

src/scale/log.js
@@ -44,13 +44,13 @@ function d3_scale_log(linear, base, positive, domain) {
   };
 
   scale.ticks = function() {
-    var extent = d3_scaleExtent(linear.domain()),
+    var extent = d3_scaleExtent(domain),
         ticks = [];
     if (extent.every(isFinite)) {
-      var i = Math.floor(extent[0]),
-          j = Math.ceil(extent[1]),
-          u = pow(extent[0]),
-          v = pow(extent[1]),
+      var i = Math.floor(log(extent[0])),
+          j = Math.ceil(log(extent[1])),
+          u = extent[0],
+          v = extent[1],
           n = base % 1 ? 2 : base;
       if (positive) {
         for (; i < j; i++) for (var k = 1; k < n; k++) ticks.push(Math.pow(base, i) * k);

test/scale/log-test.js
@@ -206,6 +206,30 @@ suite.addBatch({
           "1e+10"
         ]);
       },
+      "generates ticks that cover the domain": function(d3) {
+        var x = d3.scale.log().domain([.01, 10000]);
+        assert.deepEqual(x.ticks(20).map(x.tickFormat(20)), [
+          "1e-2", "2e-2", "3e-2", "", "", "", "", "", "",
+          "1e-1", "2e-1", "3e-1", "", "", "", "", "", "",
+          "1e+0", "2e+0", "3e+0", "", "", "", "", "", "",
+          "1e+1", "2e+1", "3e+1", "", "", "", "", "", "",
+          "1e+2", "2e+2", "3e+2", "", "", "", "", "", "",
+          "1e+3", "2e+3", "3e+3", "", "", "", "", "", "",
+          "1e+4"
+        ]);
+      },
+      "generates ticks that cover the niced domain": function(d3) {
+        var x = d3.scale.log().domain([.0124123, 1230.4]).nice();
+        assert.deepEqual(x.ticks(20).map(x.tickFormat(20)), [
+          "1e-2", "2e-2", "3e-2", "", "", "", "", "", "",
+          "1e-1", "2e-1", "3e-1", "", "", "", "", "", "",
+          "1e+0", "2e+0", "3e+0", "", "", "", "", "", "",
+          "1e+1", "2e+1", "3e+1", "", "", "", "", "", "",
+          "1e+2", "2e+2", "3e+2", "", "", "", "", "", "",
+          "1e+3", "2e+3", "3e+3", "", "", "", "", "", "",
+          "1e+4"
+        ]);
+      },
       "can override the tick format": function(d3) {
         var x = d3.scale.log().domain([1000.1, 1]);
         assert.deepEqual(x.ticks().map(x.tickFormat(10, d3.format("+,d"))), [

commit 504fc7c9e5b16ea8620cf621c59f28d71b5e64da

Fix #1420 - regression with degenerate log ticks.
Commit 8c7cf85d broke the check for a degenerate domain; this restores the
original check and adds a test to verify this behavior.

d3.js
@@ -6843,9 +6843,8 @@ d3 = function() {
       return scale;
     };
     scale.ticks = function() {
-      var extent = d3_scaleExtent(domain), ticks = [];
-      if (extent.every(isFinite)) {
-        var u = extent[0], v = extent[1], i = Math.floor(log(u)), j = Math.ceil(log(v)), n = base % 1 ? 2 : base;
+      var extent = d3_scaleExtent(domain), ticks = [], u = extent[0], v = extent[1], i = Math.floor(log(u)), j = Math.ceil(log(v)), n = base % 1 ? 2 : base;
+      if (isFinite(j - i)) {
         if (positive) {
           for (;i < j; i++) for (var k = 1; k < n; k++) ticks.push(pow(i) * k);
           ticks.push(pow(i));

src/scale/log.js
@@ -48,13 +48,13 @@ function d3_scale_log(linear, base, positive, domain) {
 
   scale.ticks = function() {
     var extent = d3_scaleExtent(domain),
-        ticks = [];
-    if (extent.every(isFinite)) {
-      var u = extent[0],
-          v = extent[1],
-          i = Math.floor(log(u)),
-          j = Math.ceil(log(v)),
-          n = base % 1 ? 2 : base;
+        ticks = [],
+        u = extent[0],
+        v = extent[1],
+        i = Math.floor(log(u)),
+        j = Math.ceil(log(v)),
+        n = base % 1 ? 2 : base;
+    if (isFinite(j - i)) {
       if (positive) {
         for (; i < j; i++) for (var k = 1; k < n; k++) ticks.push(pow(i) * k);
         ticks.push(pow(i));

test/scale/log-test.js
@@ -247,6 +247,15 @@ suite.addBatch({
           "100", "200", "300", "", "", "", "", "", "",
           "1k"
         ]);
+      },
+      "generates empty ticks when the domain is degenerate": function(d3) {
+        var x = d3.scale.log();
+        assert.deepEqual(x.domain([0, 1]).ticks(), []);
+        assert.deepEqual(x.domain([1, 0]).ticks(), []);
+        assert.deepEqual(x.domain([0, -1]).ticks(), []);
+        assert.deepEqual(x.domain([-1, 0]).ticks(), []);
+        assert.deepEqual(x.domain([-1, 1]).ticks(), []);
+        assert.deepEqual(x.domain([0, 0]).ticks(), []);
       }
     },
 